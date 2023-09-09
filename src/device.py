db0be64db48734c8d659a73862767bc7cb55407f
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-09-09 23:54:43 +0900

    add `device info` command

---
+++ src/device.py
@@ -0,0 +1,52 @@
+import lldb
+import shlex
+import argparse
+import subprocess
+import os
+from typing import Union, cast
+import util
+
+
+def __lldb_init_module(debugger: lldb.SBDebugger, internal_dict: dict) -> None:
+    debugger.HandleCommand('command script add -f device.handle_command device -h "device debugging[iLLDB]"')
+
+
+def handle_command(
+        debugger: lldb.SBDebugger,
+        command: str,
+        exe_ctx: lldb.SBExecutionContext,
+        result: lldb.SBCommandReturnObject,
+        internal_dict: dict) -> None:
+    args: Union[list[str], argparse.Namespace] = shlex.split(command, posix=False)
+    args = parse_args(cast(list[str], args))
+
+    if args.subcommand == "info":
+        info(args, debugger, result)
+
+
+def parse_args(args: list[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="UI debugging",
+                                     formatter_class=util.HelpFormatter)
+    subparsers = parser.add_subparsers(title="Subcommands", dest="subcommand")
+
+    subparsers.add_parser("info", help="Show device info")
+
+    return parser.parse_args(args)
+
+
+def info(args: argparse.Namespace, debugger: lldb.SBDebugger, result: lldb.SBCommandReturnObject) -> None:
+    file_path = os.path.realpath(__file__)
+    dir_name = os.path.dirname(file_path)
+
+    script_ret = subprocess.run(f"cat {dir_name}/swift/device.swift", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    script = script_ret.stdout
+    script += """
+    printDeviceInfo()
+    """
+
+    ret = util.exp_script(
+        debugger,
+        script
+    )
+    result.AppendMessage(ret)
---
+++ src/swift/device.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+func printDeviceInfo() {
+    let currentDevice = UIDevice.current
+
+    print("[Device Info]")
+    print("Name:              \(currentDevice.name)")
+    print("Model:             \(currentDevice.model)")
+    print("System Name:       \(currentDevice.systemName)")
+    print("System Version:    \(currentDevice.systemVersion)")
+    if let identifierForVendor = currentDevice.identifierForVendor {
+        print("Identifier (UDID): \(identifierForVendor.uuidString)")
+    }
+}
