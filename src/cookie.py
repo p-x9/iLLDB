bf010e8fb292f3ce2ad0c2b22098f34e5274f634
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-09-21 23:14:34 +0900

    implemented `cookie delete` command

--- src/cookie.py
+++ src/cookie.py
@@ -20,6 +20,13 @@ def handle_command(
 
     if args.subcommand == "read":
         read(args, debugger, result)
+    elif args.subcommand == "delete":
+        read(args, debugger, result)
+        confirm = input('The above cookies will be deleted. Please type "Yes" if OK\n')
+        if confirm == "Yes":
+            delete(args, debugger, result)
+        else:
+            print("Cancelled")
 
 
 def parse_args(args: list[str]) -> argparse.Namespace:
@@ -37,17 +44,60 @@ def parse_args(args: list[str]) -> argparse.Namespace:
     read_command.add_argument("--name", type=str, help="Name for Cookie")
     read_command.add_argument("--path", type=str, help="Path for Cookie")
 
+    # delete
+    delete_command = subparsers.add_parser("delete",
+                                           help="Delete Cookie",
+                                           formatter_class=util.HelpFormatter)
+    delete_command.add_argument("--group-id", type=str, help="AppGroup identifier for cookie storage")
+    delete_command.add_argument("--domain", type=str, help="Domain for Cookie")
+    delete_command.add_argument("--name", type=str, help="Name for Cookie")
+    delete_command.add_argument("--path", type=str, help="Path for Cookie")
+
     return parser.parse_args(args)
 
 
 def read(args: argparse.Namespace, debugger: lldb.SBDebugger, result: lldb.SBCommandReturnObject) -> None:
-    script = "@import Foundation;\n"
+    script = ""
     if args.group_id is not None:
         script += f'NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedCookieStorageForGroupContainerIdentifier:@"{args.group_id}"];\n'
     else:
         script += "NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];\n"
 
     script += util.read_script_file('objc/cookie_read.m')
+
+    # domain
+    if args.domain is not None:
+        script = script.replace("<DOMAIN>", args.domain)
+    else:
+        script = script.replace("@\"<DOMAIN>\"", "NULL")
+
+    # name
+    if args.name is not None:
+        script = script.replace("<NAME>", args.name)
+    else:
+        script = script.replace("@\"<NAME>\"", "NULL")
+
+    # path
+    if args.path is not None:
+        script = script.replace("<PATH>", args.path)
+    else:
+        script = script.replace("@\"<PATH>\"", "NULL")
+
+    _ = util.exp_script(
+        debugger,
+        script,
+        lang=lldb.eLanguageTypeObjC
+    )
+
+
+def delete(args: argparse.Namespace, debugger: lldb.SBDebugger, result: lldb.SBCommandReturnObject) -> None:
+    script = ""
+    if args.group_id is not None:
+        script += f'NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedCookieStorageForGroupContainerIdentifier:@"{args.group_id}"];\n'
+    else:
+        script += "NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];\n"
+
+    script += util.read_script_file('objc/cookie_delete.m')
 
     # domain
     if args.domain is not None:
--- src/objc/cookie_delete.m
+++ src/objc/cookie_delete.m
@@ -10,8 +10,6 @@ NSString *path = @"<PATH>";
 NSArray *allCookies = [storage cookies];
 int matchedCount = 0;
 
-printf("Cookie Information:\n");
-
 for (NSHTTPCookie *cookie in allCookies) {
   BOOL domainMatched =
       (domain == nil) || [cookie.domain isEqualToString:domain];
@@ -19,23 +17,11 @@ for (NSHTTPCookie *cookie in allCookies) {
   BOOL pathMatched = (path == nil) || [cookie.path isEqualToString:path];
 
   if (domainMatched && nameMatched && pathMatched) {
-    printf("-------------------\n");
-
-    printf("  Name:    %s\n", (char *)[cookie.name UTF8String]);
-    printf("  Value:   %s\n", (char *)[cookie.value UTF8String]);
-    printf("  Domain:  %s\n", (char *)[cookie.domain UTF8String]);
-    printf("  Path:    %s\n", (char *)[cookie.path UTF8String]);
-
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-    NSString *formattedDate = [dateFormatter stringFromDate:cookie.expiresDate];
-    printf("  Expires: %s\n", (char *)[formattedDate UTF8String]);
-
-    printf("-------------------\n");
+    [storage deleteCookie:cookie];
     matchedCount++;
   }
 }
 
 if (matchedCount == 0) {
-  printf("  None\n");
+  printf("%d Cookies was deleted\n", matchedCount);
 }
