bbc0d2b45d97ca2b5a41186bc894f85996cb71e2
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-10-03 03:35:11 +0900

    add doc string

--- src/commands.py
+++ src/commands.py
@@ -11,6 +11,16 @@ def mirror(
         result: lldb.SBCommandReturnObject,
         internal_dict: dict
 ) -> None:
+    """
+    Display the child elements of an object using the Mirror class in Swift.
+
+    Args:
+        debugger (lldb.SBDebugger): The LLDB debugger object.
+        command (str): The command string passed to the mirror command.
+        exe_ctx (lldb.SBExecutionContext): The execution context of the command.
+        result (lldb.SBCommandReturnObject): The object used to return the result of the command.
+        internal_dict (dict): The internal dictionary of the command.
+    """
     args = shlex.split(command, posix=False)
     if len(args) < 1:
         return
